295ffe635a85ed985349b469bd8165b110f90e1c
Pwootage <pwootage@Pwootages-MacBook-Pro.local>
2014-07-25 00:30:07 -0600

    Add counts, google analytics tracker
    I’d like to know if it’s used ;)

--- js/helper.js
+++ js/helper.js
@@ -35,6 +35,62 @@ var pageController = helperApp.controller('PageController', ['$scope', '$http', 
     return ret;
   };
 
+  $scope.itemFoundCount = function() {
+    var count = 0;
+    angular.forEach($scope.items, function (value, key) {
+      if (value.found) {
+        count += value.found;
+      }
+    });
+    return count;
+  };
+  $scope.itemRemCount = function() {
+    var total = 0;
+    var count = 0;
+    angular.forEach($scope.items, function (value, key) {
+      total += value.count;
+      if (value.found) {
+        count += value.found;
+      }
+    });
+    return total - count;
+  };
+  $scope.itemTotalCount = function() {
+    var total = 0;
+    angular.forEach($scope.items, function (value, key) {
+      total += value.count;
+    });
+    return total;
+  };
+
+  $scope.locationFoundCount = function() {
+    var count = 0;
+    angular.forEach($scope.locations, function (value, key) {
+      if (value.actualItem) {
+        count++;
+      }
+    });
+    return count;
+  };
+  $scope.locationRemCount = function() {
+    var total = 0;
+    var count = 0;
+    angular.forEach($scope.locations, function (value, key) {
+      total += 1;
+      if (value.actualItem) {
+        count++;
+      }
+    });
+    return total - count;
+  };
+  $scope.locationTotalCount = function() {
+    var total = 0;
+    angular.forEach($scope.locations, function (value, key) {
+      total += 1;
+    });
+    return total;
+  };
+
   if (localStorage['locations']) {
     $scope.locations = JSON.fromJson(localStorage['locations']);
     updateItems();

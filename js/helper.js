aaf4aaef7a720ddfb1f77152f92bd3458651b3a7
Pwootage <pwootage@Pwootages-MacBook-Pro.local>
2014-07-25 20:44:05 -0600

    Saving between sessions, obtained button, formatting

--- js/helper.js
+++ js/helper.js
@@ -9,17 +9,33 @@ var pageController = helperApp.controller('PageController', ['$scope', '$http', 
     console.log("Updating items");
     angular.forEach($scope.items, function (value, key) {
       value.found = 0;
+      value.obtained = 0;
     });
+    var itemsTotal = 0;
+    var itemsFound = 0;
+    var itemsObtained = 0;
     angular.forEach($scope.locations, function (value, key) {
+      itemsTotal++;
       var itemFound = value.actualItem;
-      if (itemFound) {
+      if (itemFound && $scope.items[itemFound]) {
+        itemsFound++;
         $scope.items[itemFound].found++;
+        if (value.obtained) {
+          itemsObtained++;
+          $scope.items[itemFound].obtained++;
+        }
       }
     });
+    $scope.itemFoundCount = itemsFound;
+    $scope.itemObtainedCount = itemsObtained;
+    $scope.itemTotalCount = itemsTotal;
   }
 
   $scope.locations = [];
   $scope.$watch('locations', function (newVal, oldVal) {
+    if (newVal !== undefined && newVal.length !== 0) {
+      localStorage['locations'] = JSON.stringify(newVal);
+    }
     updateItems();
   }, true);
 
@@ -35,70 +51,61 @@ var pageController = helperApp.controller('PageController', ['$scope', '$http', 
     return ret;
   };
 
-  $scope.itemFoundCount = function() {
-    var count = 0;
-    angular.forEach($scope.items, function (value, key) {
-      if (value.found) {
-        count += value.found;
-      }
-    });
-    return count;
-  };
-  $scope.itemRemCount = function() {
-    var total = 0;
-    var count = 0;
-    angular.forEach($scope.items, function (value, key) {
-      total += value.count;
-      if (value.found) {
-        count += value.found;
-      }
-    });
-    return total - count;
-  };
-  $scope.itemTotalCount = function() {
-    var total = 0;
-    angular.forEach($scope.items, function (value, key) {
-      total += value.count;
-    });
-    return total;
+  $scope.itemFoundCount = 0;
+  $scope.itemObtainedCount = 0;
+  $scope.itemTotalCount = 0;
+
+  $scope.buttonText = function (obtained) {
+    if (obtained) {
+      return 'Obtained';
+    } else {
+      return 'Found';
+    }
   };
 
-  $scope.locationFoundCount = function() {
-    var count = 0;
-    angular.forEach($scope.locations, function (value, key) {
-      if (value.actualItem) {
-        count++;
-      }
-    });
-    return count;
-  };
-  $scope.locationRemCount = function() {
-    var total = 0;
-    var count = 0;
-    angular.forEach($scope.locations, function (value, key) {
-      total += 1;
-      if (value.actualItem) {
-        count++;
-      }
-    });
-    return total - count;
-  };
-  $scope.locationTotalCount = function() {
-    var total = 0;
-    angular.forEach($scope.locations, function (value, key) {
-      total += 1;
-    });
-    return total;
+  $scope.locationShow = 'all';
+  $scope.shouldShowLocation = function (loc) {
+    if ($scope.locationShow === 'all') {
+      return true
+    } else if ($scope.locationShow === 'found' && loc.actualItem && loc.actualItem !== '') {
+      return true;
+    } else if ($scope.locationShow === 'remaining' && (loc.actualItem === undefined || loc.actualItem === '')) {
+      return true;
+    }
+    return false;
   };
 
-  if (localStorage['locations']) {
-    $scope.locations = JSON.fromJson(localStorage['locations']);
-    updateItems();
-  } else {
+  $scope.itemShow = 'all';
+  $scope.shouldShowItem = function (item) {
+    if ($scope.itemShow === 'all') {
+      return true
+    } else if ($scope.itemShow === 'found' && item.found > 0) {
+      return true;
+    } else if ($scope.itemShow === 'remaining' && item.obtained < item.count) {
+      return true;
+    }
+    return false;
+  };
+
+  function resetImpl() {
     $http.get('../js/locations.json').success(function (data) {
       $scope.locations = data;
       updateItems();
     });
+  }
+
+  $scope.reset = function () {
+    var reallyDo = confirm('Are you sure you want to reset? This cannot be undone!');
+    if (reallyDo) {
+      resetImpl();
+    }
+  };
+
+  if (localStorage['locations']) {
+    $scope.locations = JSON.parse(localStorage['locations']);
+    updateItems();
+  } else {
+    resetImpl();
   }
 
   $http.get('../js/items.json').success(function (data) {
